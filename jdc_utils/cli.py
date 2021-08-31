947ac7617b6b3e995101b62a9fe410c8071dfb17
Phil Schumm <pschumm@uchicago.edu>
2021-08-31 07:07:42 -0400

    Initial commit

---
+++ jdc_utils/cli.py
@@ -0,0 +1,12 @@
+"""CLI for JDC utilities"""
+
+import click
+
+@click.group()
+def cli():
+    """CLI for JDC utilities"""
+    pass
+
+
+if __name__ == '__main__':
+    cli()
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='jdc-utils',
+    version='0.0.1',
+    author='Phil Schumm',
+    author_email='pschumm@uchicago.edu',
+    description='Utilities for preparing and submitting data to the JDC',
+    url='https://rcg.bsd.uchicago.edu/gitlab/maarc/dasc/jdc-utilities',
+    packages=find_namespace_packages(where='.'),
+    install_requires=[
+        'dataforge',
+    ],
+    entry_points='''
+        [console_scripts]
+        jdc-utils=jdc_utils.cli:cli
+    ''',
+)
