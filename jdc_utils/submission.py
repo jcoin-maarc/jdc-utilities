d08e1fc03d373eb0ecd03c95c0a1b812ff605802
Phil Schumm <pschumm@uchicago.edu>
2021-09-07 07:11:31 -0500

    Add module for generating submissions

---
+++ jdc_utils/submission.py
@@ -0,0 +1,69 @@
+"""Generate TSV files for submission to JDC"""
+
+from urllib.request import urlopen
+import json
+import sys
+
+# TODO Replace with version-free link to current dictionary
+DICTIONARY_URL = 'https://dictionary-artifacts.s3.amazonaws.com/jcoin_datadictionary/1.1.1/schema.json'
+
+class Node:
+    
+    def __init__(self, type):
+        
+        self.type = type
+        
+        response = urlopen(DICTIONARY_URL)
+        dictionary = json.loads(response.read())
+        
+        self.system_properties = dictionary[f'{type}.yaml']['systemProperties']
+        self.properties = dictionary[f'{type}.yaml']['properties']
+        self.links = dictionary[f'{type}.yaml']['links']
+        self.required = dictionary[f'{type}.yaml']['required']
+    
+    def _validate(self, df):
+        
+        exclude = ['type','submitter_id']
+        properties = [p for p in self.properties.keys()
+                      if p not in self.system_properties + exclude]
+        required = [p for p in self.required if p not in exclude]
+        
+        for property in properties:
+            if property in required:
+                if property not in df:
+                    sys.exit(f'Column {property} required but not found')
+                elif df[property].isnull().values.any():
+                    sys.exit(f'Property {property} required but missing in one or more cases')
+            
+            if property in df and 'enum' in self.properties[property]:
+                valid = set(self.properties[property]['enum'])
+                if not set(df[property].dropna().unique()).issubset(valid):
+                    invalid = set(df[property].dropna().unique()) - valid
+                    sys.exit(f'Value(s) {invalid} not valid for {property}')
+    
+    def to_tsv(self, df, path_or_buf, submitter_id=None, constants=None):
+        
+        data = df.copy()
+        if constants:
+            for c in constants:
+                data[c] = constants[c]
+        self._validate(data)
+        
+        exclude = ['type','submitter_id']
+        cols = [p for p in self.properties.keys()
+                if p not in self.system_properties + exclude]
+        if submitter_id:
+            cols.append(submitter_id)
+        data = data.loc[:,data.columns.isin(cols)]
+        
+        if submitter_id:
+            data.set_index(submitter_id, inplace=True, verify_integrity=True)
+        data.index.rename('submitter_id', inplace=True)
+        
+        for link in self.links:
+            if link['name'] in data.columns:
+                data.rename(columns={link['name']:f"{link['name']}.submitter_id"},
+                            inplace=True)
+        
+        data.insert(0, 'type', self.type)
+        data.to_csv(path_or_buf, sep='\t')
--- jdc_utils/transforms.py
+++ jdc_utils/transforms.py
@@ -7,49 +7,34 @@ def read_mapfile(mapfile):
     with open(mapfile) as file:
         return yaml.safe_load(file)
 
-def map(df, mappings):
+def map(df, mapfile):
     """Rename vars and/or replace values"""
-    for var in mappings:
-        if var in df:
-            if 'values' in mappings[var]:
-                df[var].replace(mappings[var]['values'], inplace=True)
-
-            if 'name' in mappings[var]:
-                df.rename(columns={var: mappings[var]['name']}, inplace=True)
+    
+    map = read_mapfile(mapfile)
+    for var in map:
+        df.rename(columns={var: map[var]['name']}, inplace=True)
+        if 'values' in map[var] and map[var]['name'] in df:
+            df[map[var]['name']].replace(map[var]['values'], inplace=True)
 
 def to_quarter(datevar):
     
     var = pd.to_datetime(datevar)
     return pd.PeriodIndex(var, freq='Q')
 
-def get_mappings(mappings,substr,mapping_property='name'):
-    '''
-    get a list of remapped names 
-    based on a substring pattern of unmapped names
-
-    returns a list of mapped names
-
-    TODO: specifying whether name or value mapping list etc
-    '''
-    remapped_name_list = [
-        name[mapping_property]
-        for key,name in mappings.items()
-        if substr in key
-    ]
-    return remapped_name_list
-
-def combine_checkboxes(
-    df,
-    name_if_more_than_one_checked,
-    value_of_checked="Checked",
-    value_of_none_checked='Not reported'
-    ):
-    num_checked = (df==value_of_checked).sum()
-    if num_checked>1:
-        race_enum = name_if_more_than_one_checked
-    elif num_checked==0:
-        race_enum = value_of_none_checked
-    else:
-        race_enum = df.loc[df==value_of_checked].index[0]
-
-    return race_enum
+def collapse_checkall(df, columns, checked='Checked',
+                      multi_checked='Multiple checked',
+                      none_checked='None checked', fillna=False, labels=None):
+    """Collapse multiple check-all-that-apply fields into one"""
+    
+    bcols = df[columns]==checked
+    var = bcols.idxmax(axis=1).where(bcols.sum(axis=1)==1, multi_checked).\
+                               where(bcols.sum(axis=1)>0, none_checked).\
+                               where(df[columns].notnull().sum(axis=1)>0, None)
+    
+    if labels:
+        var.replace(dict(zip(columns,labels)), inplace=True)
+    
+    if fillna:
+        var.fillna(fillna, inplace=True)
+    
+    return var
