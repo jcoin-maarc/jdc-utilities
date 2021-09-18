aac94cf80836378e7ee89bab2e41eaa80fa1484c
mbkranz <kranz-michael@norc.org>
2021-09-17 19:00:27 -0500

    added to fxns to Node class for adding cols

--- jdc_utils/submission.py
+++ jdc_utils/submission.py
@@ -5,6 +5,7 @@ import json
 import sys
 from jdc_utils.dictionary import build_schema,get_dictionary
 from jdc_utils.transforms import map as map_jdc
+from jdc_utils.transforms import to_quarter
 
 #manifest used for production deployment
 MANIFEST_URL = 'https://raw.githubusercontent.com/uc-cdis/cdis-manifest/master/jcoin.datacommons.io/manifest.json'
@@ -30,13 +31,35 @@ class Node:
             self.unique_keys,
             self.system_properties
         )
-    
-    def map_df(self,df,mapfile,to_tsv=False):
+
+    def map_df(self,df,mapfile):
         data = df.copy()
         map_jdc(data, mapfile)
-        cols = self.schema.columns
-        data = data.loc[:,data.columns.isin(cols)]
-        self.data = data
-        self.validated_data = self.schema.validate(self.data)
-        if to_tsv:
-            self.validated_data.to_csv(path_or_buf, sep='\t')
+        data.insert(0,'type',self.type)
+        self.unvalidated_data = data
+
+    def add_submitter_ids(self,ids,parent_node=None):
+        #TODO: replace_id function from dataforge here
+        if parent_node:
+            self.unvalidated_data[f"{parent_node}.submitter_id"] = ids
+        else:
+            self.unvalidated_data["submitter_id"] = ids
+
+    def add_quarter(self,from_column='date_recruited'):
+        self.unvalidated_data['quarter_recruited'] = to_quarter(
+            self.unvalidated_data.date_recruited
+            ).fillna('Not reported')
+
+    def add_role_in_project(self,role):
+        self.unvalidated_data['role_in_project'] = role
+
+    def validate_df(self):
+        cols = self.schema.columns.keys()
+        node_cols = self.unvalidated_data.columns.isin(cols)
+        node_data = self.unvalidated_data.loc[:,node_cols]
+        self.validated_data = self.schema.validate(node_data)
+
+    def to_tsv(self,file_dir,file_path):
+        Path(file_dir).mkdir(parents=True, exist_ok=True)
+        self.validated_data.to_csv(os.path.join(file_dir,file_name))
+
