e84574d5fc46d64dd5a1eaf4b6d68ac17cecc023
mbkranz <kranz-michael@norc.org>
2021-09-18 13:05:57 -0500

    updated class Node --> NodeSubmission; added NodeDictionary class
    dictionary functions into NodeDictionary class, NodeSubmission inherits NodeDictionary; other updates to support chaining

--- jdc_utils/submission.py
+++ jdc_utils/submission.py
@@ -3,7 +3,7 @@
 from urllib.request import urlopen
 import json
 import sys
-from jdc_utils.dictionary import build_schema,get_dictionary
+from jdc_utils.dictionary import NodeDictionary
 from jdc_utils.transforms import map as map_jdc
 from jdc_utils.transforms import to_quarter
 
@@ -11,32 +11,17 @@ from jdc_utils.transforms import to_quarter
 MANIFEST_URL = 'https://raw.githubusercontent.com/uc-cdis/cdis-manifest/master/jcoin.datacommons.io/manifest.json'
 
 
-class Node:
+class NodeSubmission(NodeDictionary):
     
-    def __init__(self, type):
-        
-        self.type = type
-        dictionary = get_dictionary(MANIFEST_URL) #note - full dictionary will be needed for node refs in future versions
-        
-        self.system_properties = dictionary[f'{type}.yaml']['systemProperties']
-        self.properties = dictionary[f'{type}.yaml']['properties']
-        self.links = dictionary[f'{type}.yaml']['links']
-        self.required = dictionary[f'{type}.yaml']['required']
-        self.unique_keys = dictionary[f'{type}.yaml']['uniqueKeys']
-
-        self.schema = build_schema(
-            self.properties,
-            self.links,
-            self.required,
-            self.unique_keys,
-            self.system_properties
-        )
+    def __init__(self, type,manifest_url=MANIFEST_URL):
+        super().__init__(manifest_url=MANIFEST_URL,type=type)
 
     def map_df(self,df,mapfile):
         data = df.copy()
         map_jdc(data, mapfile)
         data.insert(0,'type',self.type)
         self.unvalidated_data = data
+        return self
 
     def add_submitter_ids(self,ids,parent_node=None):
         #TODO: replace_id function from dataforge here
@@ -44,20 +29,24 @@ class Node:
             self.unvalidated_data[f"{parent_node}.submitter_id"] = ids
         else:
             self.unvalidated_data["submitter_id"] = ids
+        return self
 
     def add_quarter(self,from_column='date_recruited'):
         self.unvalidated_data['quarter_recruited'] = to_quarter(
             self.unvalidated_data.date_recruited
             ).fillna('Not reported')
+        return self
 
     def add_role_in_project(self,role):
         self.unvalidated_data['role_in_project'] = role
+        return self
 
     def validate_df(self):
         cols = self.schema.columns.keys()
         node_cols = self.unvalidated_data.columns.isin(cols)
         node_data = self.unvalidated_data.loc[:,node_cols]
         self.validated_data = self.schema.validate(node_data)
+        return self
 
     def to_tsv(self,file_dir,file_path):
         Path(file_dir).mkdir(parents=True, exist_ok=True)
