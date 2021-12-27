c459f0f018d44b90c4729df84828b02dd4fa3490
mbkranz <kranz-michael@norc.org>
2021-12-27 10:31:50 -0600

    kwargs to read_df

--- jdc_utils/transforms.py
+++ jdc_utils/transforms.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 import pandas_flavor as pf
 from pathlib import Path
 
-def read_df(file_path):
+def read_df(file_path,**kwargs):
     '''
     read in a data file based on
     the type of file
@@ -18,13 +18,13 @@ def read_df(file_path):
     file_type = re.split("\.",str(file_path))[-1]
 
     if file_type=='csv':
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(file_path,**kwargs)
     elif file_type=='tsv':
-        df = pd.read_csv(file_path,sep='\t')
+        df = pd.read_csv(file_path,sep='\t',**kwargs)
     elif file_type=='xlsx':
-        df = pd.read_excel(file_path,engine='openpyxl')
+        df = pd.read_excel(file_path,engine='openpyxl',**kwargs)
     elif file_type=='xls':
-        df = pd.read_excel(file_path,engine='xlrd')
+        df = pd.read_excel(file_path,engine='xlrd',**kwargs)
     #TO ADD:
     #redcap 
     else:
