f29b4021d3b038bbac1b253a85f841292e1877da
mbkranz <kranz-michael@norc.org>
2022-07-07 08:17:22 -0500

    update pkg versions in setup.py

--- setup.py
+++ setup.py
@@ -10,18 +10,19 @@ setup(
     packages=find_namespace_packages(where='.'),
     package_data={'jdc_utils': ['frictionless/table_schemas/table_schema_urls.yaml']},
     install_requires=[
-        'pandas==1.4.1',
-        'pandas_flavor==0.2.0',
-        'gitpython==3.1.27',
-        'click==7.1.2',
-        'openpyxl==3.0.9',
-        'xlrd==2.0.1',
-        'pyyaml==5.4.1',
-        'pandera==0.9.0',
-        'jsonpath_ng==1.5.3',
-        'requests==2.27.1',
-        'frictionless==4.26.0',
-        'gen3==4.4.0'
+        'pandas>=1.4.1',
+        'pandas_flavor>=0.2.0',
+        'gitpython>=3.1.27',
+        'click>=8.1.2',
+        'openpyxl>=3.0.9',
+        'xlrd>=2.0.1',
+        'pyyaml>=5.4.1',
+        'pandera>=0.9.0',
+        'jsonpath_ng>=1.5.3',
+        'requests>=2.27.1',
+        'frictionless>=4.26.0',
+        'gen3>=4.4.0',
+        'pyreadstat>=1.1.7'
     ],
     entry_points='''
         [console_scripts]
