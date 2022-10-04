209fc24c05809cdd307e806c9c815d7195bb6401
mbkranz <kranz-michael@norc.org>
2022-10-04 10:28:48 -0500

    update schema refs to new repo name

--- jdc_utils/schema.py
+++ jdc_utils/schema.py
@@ -4,6 +4,6 @@ from types import SimpleNamespace
 from frictionless import Schema
 
 core_measures = SimpleNamespace(
-    baseline = Schema('https://raw.githubusercontent.com/jcoin-maarc/JCOIN_frictionless_dictionary/pschumm/reorg/schemas/table-schema-baseline.json'),
-    timepoints = Schema('https://raw.githubusercontent.com/jcoin-maarc/JCOIN_frictionless_dictionary/pschumm/reorg/schemas/table-schema-time-points.json')
+    baseline = Schema('https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/pschumm/reorg/schemas/table-schema-baseline.json'),
+    timepoints = Schema('https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/pschumm/reorg/schemas/table-schema-time-points.json')
 )
