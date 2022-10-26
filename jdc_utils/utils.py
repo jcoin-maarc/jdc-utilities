2dd746badead08ee1ac0d7ad8c0054694c394c1e
mbkranz <kranz-michael@norc.org>
2022-10-26 08:06:22 -0500

    add new utils for marginals submissions

--- jdc_utils/utils.py
+++ jdc_utils/utils.py
@@ -116,3 +116,92 @@ def df_from_marginals(filepath, layout_path, fillna=False, order='random',
     
     df = pd.concat(cols, axis=1)
     return df
+
+
+
+
+def split_marginals_and_total(df:pd.DataFrame) -> pd.DataFrame:
+    """split into total and marginals
+
+    Args:
+        df (pd.DataFrame): _description_
+
+    Returns:
+        tuple[pd.DataFrame,pd.DataFrame]: _description_
+    """
+    totals = df.loc[df.index=='Total']
+    marginals = df.loc[df.index!='Total']
+    totals_diff = totals - marginals.sum()
+    totals_exceed = totals_diff<0
+    # check totals
+    exit = False
+    if totals_exceed.sum().sum()<0:
+        mess = (
+            f'For {sheet_name}:\n'
+            'the sum of marginals exceed the reported total number\n'
+        )
+        print('{sheet_name}')
+        exit = True 
+
+    if exit:
+        sys.exit('Correct errors and try again')
+
+    # add not reported (happens if cell size restrictions)
+    marginals.loc['Not reported',:] = totals_diff.values
+
+    return marginals
+
+
+# create individual level data
+
+# multiply category by count
+def create_marginal_sample(marginals:pd.DataFrame,column_name:str='column',category_name:str='category') -> pd.DataFrame:
+    """Create the marginal sample (or individual level data)
+    by multiplying the category names by the total count for that
+    category
+
+
+    Args:
+        marginal (pd.DataFrame): _description_
+        column_name (str): _description_
+        category_name (str): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    sample = []
+    for col,marginal in marginals.iteritems():
+        for category,total in marginal.iteritems():
+            if pd.notna(total):
+                sample.extend(int(total)*[{column_name:col,category_name:category}])
+    random.shuffle(sample)
+    return pd.DataFrame(sample)
+
+def _create_sample(xls_path,sheet_name):
+    sample_df = (
+        pd.read_excel(DATA_PATH,sheet_name=sheet_name)
+        .pipe(
+            lambda df: df.set_index(df.columns[0])
+        )
+        .pipe(
+            split_marginals_and_total
+        )
+        .pipe(
+            create_marginal_sample
+        )
+        .pipe(
+            lambda df: df.set_index(df.columns[0],append=True)
+
+        )
+    )
+    return sample_df
+def create_gen3_submission(xls_path:str,category_name_mappings:dict) -> pd.DataFrame:
+    df = pd.DataFrame([],index=[None,'category'])
+    df = pd.concat([
+    _create_sample(DATA_PATH, sheet_name)\
+        .rename(columns={'category':gen3_name})\
+    for sheet_name,gen3_name in category_name_mappings.items()],
+    axis=1)
+    return df
+
+    
