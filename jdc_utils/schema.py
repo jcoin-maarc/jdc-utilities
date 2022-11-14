7dbdfb85a54f7cc96b82a89a8894a106641ef06a
mbkranz <kranz-michael@norc.org>
2022-11-14 14:27:01 -0600

    Changed schema reference to master branch
    NOTE: merged reserve code edits of JCOIN-Core-Measures into main branch (see other closed issues in JCOIN-Core-Measures repo integrated)

--- jdc_utils/schema.py
+++ jdc_utils/schema.py
@@ -3,7 +3,8 @@
 from types import SimpleNamespace
 from frictionless import Schema
 
+branch = 'master'
 core_measures = SimpleNamespace(
-    baseline = Schema('https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/pschumm/reorg/schemas/table-schema-baseline.json'),
-    timepoints = Schema('https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/pschumm/reorg/schemas/table-schema-time-points.json')
+    baseline = Schema(f'https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/{branch}/schemas/table-schema-baseline.json'),
+    timepoints = Schema(f'https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/{branch}/schemas/table-schema-time-points.json')
 )
