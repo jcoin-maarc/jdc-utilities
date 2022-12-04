246d4fcc280d5784d05cbafcfccd58189c27b9cb
mbkranz <kranz-michael@norc.org>
2022-12-04 14:03:25 -0600

    added frictionless stata/spss plugins from dataforge fork

--- jdc_utils/__init__.py
+++ jdc_utils/__init__.py
@@ -1,2 +1,6 @@
 from . import submission
 from . import transforms
+
+#frictionless plugins
+from dataforge.frictionless import frictionless_dataforgespss
+from dataforge.frictionless import frictionless_dataforgestata
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_namespace_packages
-
+from pathlib import Path
+requirements = Path("requirements.txt").open().readlines()
 setup(
     name='jdc-utils',
     version='0.0.1',
@@ -9,22 +10,7 @@ setup(
     url='https://rcg.bsd.uchicago.edu/gitlab/maarc/dasc/jdc-utilities',
     packages=find_namespace_packages(where='.'),
     package_data={'jdc_utils': ['frictionless/table_schemas/table_schema_urls.yaml']},
-    install_requires=[
-        'pandas>=1.4.1',
-        'pandas_flavor>=0.2.0',
-        'gitpython>=3.1.27',
-        'click>=8.1.2',
-        'openpyxl>=3.0.9',
-        'xlrd>=2.0.1',
-        'pyyaml>=5.4.1',
-        'pandera>=0.9.0',
-        'jsonpath_ng>=1.5.3',
-        'requests>=2.27.1',
-        'frictionless>=4.26.0',
-        'gen3>=4.4.0',
-        'pyreadstat>=1.1.7',
-        'dataforge @ git+https://gitlab.com/phs-rcg/data-forge.git'
-    ],
+    install_requires=requirements,
     entry_points='''
         [console_scripts]
         jdc-utils=jdc_utils.cli:cli
