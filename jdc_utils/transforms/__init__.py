956c4dd47a15130319dd7fdd0b528c9d64acf408
mbkranz <kranz-michael@norc.org>
2023-01-12 11:57:12 -0600

    init transform reorg

---
+++ jdc_utils/transforms/__init__.py
@@ -0,0 +1,32 @@
+from transforms.io import *
+from trnasforms.methods import *
+
+def read_transformfile(transformfile):
+    with open(transformfile) as file:
+        return yaml.safe_load(file)
+
+
+def run_transformfile(df, transformfile):
+    """
+    loop through transformations and mappings as specified
+    in the yaml file
+
+    then runs a given function name with a set of paramaters.
+    Intended to transform the dataframe inplace.
+    To provide compatability with native pandas fxns,
+    the inplace argument assumed to be a parameter.
+
+    If kwargs, then need to register a function
+    calling the dictionary as keyword args in TransformDf class.
+    """
+    transform_mappings = OrderedDict(read_transformfile(transformfile))
+
+    for fxn_name, params in transform_mappings.items():
+        if fxn_name in ['replace_ids','shift_dates']:
+            print(fxn_name)
+            df = getattr(df, fxn_name)(**params)
+            print(df.columns)
+        else:
+            getattr(df, fxn_name)(**params)
+    
+    return df 
--- jdc_utils/transforms/methods.py
+++ jdc_utils/transforms/methods.py
@@ -1,5 +1,3 @@
-"""Data transformations"""
-
 import pandas as pd
 import openpyxl
 import xlrd
@@ -10,60 +8,6 @@ import pandas_flavor as pf
 from pathlib import Path
 import jdc_utils.dataforge_tools as tools 
 import jdc_utils.dataforge_ids as ids 
-
-
-def read_df(file_path, **kwargs):
-    """
-    read in a data file based on
-    the type of file
-
-    """
-    file_type = re.split("\.", str(file_path))[-1]
-
-    if file_type == "csv":
-        df = pd.read_csv(file_path, **kwargs)
-    elif file_type == "tsv":
-        df = pd.read_csv(file_path, sep="\t", **kwargs)
-    elif file_type == "xlsx":
-        df = pd.read_excel(file_path, engine="openpyxl", **kwargs)
-    elif file_type == "xls":
-        df = pd.read_excel(file_path, engine="xlrd", **kwargs)
-    # TO ADD:
-    # redcap, from internet
-    else:
-        sys.exit("Data type not supported")
-    return df
-
-
-def read_transformfile(transformfile):
-    with open(transformfile) as file:
-        return yaml.safe_load(file)
-
-
-def run_transformfile(df, transformfile):
-    """
-    loop through transformations and mappings as specified
-    in the yaml file
-
-    then runs a given function name with a set of paramaters.
-    Intended to transform the dataframe inplace.
-    To provide compatability with native pandas fxns,
-    the inplace argument assumed to be a parameter.
-
-    If kwargs, then need to register a function
-    calling the dictionary as keyword args in TransformDf class.
-    """
-    transform_mappings = OrderedDict(read_transformfile(transformfile))
-
-    for fxn_name, params in transform_mappings.items():
-        if fxn_name in ['replace_ids','shift_dates']:
-            print(fxn_name)
-            df = getattr(df, fxn_name)(**params)
-            print(df.columns)
-        else:
-            getattr(df, fxn_name)(**params)
-    
-    return df 
 
 
 # Note: alternative to using pandas-flavor is making a child class of pd.DataFrame
