feca6e6a73c4e86597dba28bbd3e5f7a15c4be43
Michael Kranz <mbkranz@gmail.com>
2023-01-17 15:13:34 -0800

    Changed defaults and params of replace_ids; Added v1 replace id documentation with new params

--- jdc_utils/transforms/deidentify.py
+++ jdc_utils/transforms/deidentify.py
@@ -7,13 +7,37 @@ from pathlib import Path
 from dataforge import tools,ids 
 
 @pf.register_dataframe_method
-def replace_ids(df, id_file, map_file, map_url=None, id_column=None):
+def replace_ids(df, id_file, id_column,id_history_path):
+    """ 
+    (pulls in the most up-to-date mappings stored in id_history_path).
+    The id_history_path pulls in the most up to date id mappings. It is stored
+    in a common/secure location such as a lab or institute server (e.g., P,J,etc drive)
+
+    1. Pulls in the most up-to-date version of previously mapped ids (called id_history_path).
+    2. Maps any new ids to local ids (ie id_column) and adds these to the mapping file.
+    3. Stores these mappings in a temporary file in tmp/git/ids.csv) which is then added (ie pushed) to the 
+    version control history in id_history_path. 
+
+
+    NOTES
+    
+    - id_history_path is a git bare repository, meaning this directory contains the entire history for mapped ids including
+        the most recent version. 
+    - The name of the new id column is determined by the column name in id_file.
+
+    - See the [dataforge function documentation](https://gitlab.com/phs-rcg/data-forge/-/blob/main/src/dataforge/ids.py) for more general details
+        on the replace id function used in this implementation.
+
+    
+    """
+
     df_new = ids.replace_ids(
         df=df, 
-        id_file=id_file, 
-        map_file=map_file,
-        map_url=None, 
-        column=id_column)
+        id_file=id_file,
+        map_file='ids.csv',
+        column=id_column,
+        map_url=id_history_path)
+
     return df_new
 
 
