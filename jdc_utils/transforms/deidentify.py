0d8947938ffaea25eda880f915875846efddc005
Michael Kranz <mbkranz@gmail.com>
2023-01-17 15:11:58 -0800

    Changed params of shift dates  this specific implementation

--- jdc_utils/transforms/deidentify.py
+++ jdc_utils/transforms/deidentify.py
@@ -17,32 +17,26 @@ def replace_ids(df, id_file, map_file, map_url=None, id_column=None):
     return df_new
 
 
+
 @pf.register_dataframe_method
 def shift_dates(
         df,
         id_column,
         date_columns,
-        map_file,
-        map_url=None,
+        shift_days_history_path,
         shift_days_col='days_for_shift_date'
 ):
-    #create/save offsets (ie shift amounts)
-    #TODO: add/test offset url and ensure map file works
-    ids = df[id_column]
+    """ 
+    
+    """
 
-    if Path(map_file).exists():
-        map_df = pd.read_csv(map_file)
-        if shift_days_col in map_df:
-            offsets = map_df.set_index(id_column)[shift_days_col]
-            offsets.to_csv(shift_days_col+'.csv')
-        else:
-            offsets = None
+    ids = df[id_column]
 
     offsets = tools.date_offset(
         key=ids,
         offset_file=shift_days_col+'.csv',
         name=shift_days_col,
-        offset_url=map_url
+        offset_url=shift_days_history_path
     )
 
     date_columns = [date_columns] if isinstance(date_columns,str) else date_columns
