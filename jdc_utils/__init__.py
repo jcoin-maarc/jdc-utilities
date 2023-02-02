6e619304ce6c41c11b287fec32ebc45caf5b5d74
mbkranz <kranz-michael@norc.org>
2023-02-02 11:20:48 -0600

    config in init

--- jdc_utils/__init__.py
+++ jdc_utils/__init__.py
@@ -1,6 +1,13 @@
 from . import submission
 from . import transforms
-
 #frictionless plugins
 from dataforge.frictionless import frictionless_dataforgespss
 from dataforge.frictionless import frictionless_dataforgestata
+
+import confuse
+
+config = confuse.Configuration('jdc-utils',__name__)
+try:
+    config.set_file('config.yaml')
+except:
+    pass 
