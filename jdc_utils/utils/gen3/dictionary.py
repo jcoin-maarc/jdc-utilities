daa780c54d1cd682c44bce13e2a3eb885fb86fe7
mbkranz <kranz-michael@norc.org>
2023-04-01 15:33:41 -0500

    frictionless isntead of pandera -- unclear about interoperability between two

--- jdc_utils/utils/gen3/dictionary.py
+++ jdc_utils/utils/gen3/dictionary.py
@@ -2,7 +2,7 @@
 import json 
 import jsonschema
 import requests
-
+from frictionless import Resource,Schema
 
 # submission validation utilities for gen3 sheepdog validation 
 def get_dictionary(endpoint,node_type='_all'):
@@ -16,7 +16,7 @@ def get_dictionary(endpoint,node_type='_all'):
     dictionary = json.loads(output)
     return dictionary
 
-class Node:
+class Gen3Node:
     '''
     object represents a gen3 node with gen3 specific properties 
     and standard json schema properties for:
@@ -24,7 +24,7 @@ class Node:
         2. writing a validated set of records to specified file format
         3. node property look up
     '''
-    def __init__(self,endpoint,type):
+    def __init__(self,endpoint,type,credential_path=None):
         self.type = type
         dictionary = get_dictionary(endpoint,type) #note - full dictionary will be needed for node refs in future versions
         
@@ -33,33 +33,104 @@ class Node:
         self.required = dictionary['required']
         self.unique_keys = dictionary['uniqueKeys']
         #get just the link names
-        self.link_names = [x.value for x in parse("$..name").find(self.links)]
+        self.link_names = [link["name"] for link in self.links if link.get("name")]
 
-        self.schema = pa.DataFrameSchema.from_schema(dictionary)
+        if credential_path:
+            self.credential_path = credential_path
 
-
-        return report
-
-    def write(self,df,return_type="records"):
-        ''' 
-        validates and outputs to specified format 
-        (ie file, json array of records, or printed tsv string)
-        '''
-        data = df.copy()
-        #make submitter_id index if not already index\
-        index_name = "submitter_id"
-
-        if data.index.name!=index_name and index_name in data.columns:
-            data.set_index(index_name,inplace=True)
+        self.frictionless_schema = {"fields":[],
+        "primaryKey":["submitter_id"],
+        "description":dictionary.get("description"),
+        "title":dictionary.get("title")}
         
-        validated_data = self.schema.validate(df)
+        for prop in self.system_properties:
+            del dictionary[prop]
+        
+        for propname,prop in list(dictionary):
+            frictionless_prop = {
+                "name":propname,
+                "description":propname.get("description")} 
+    
+            # type
+            if prop.get("type"):
+                if isinstance(prop["type"],list):
+                    # NOTE: frictionless currently doesnt supprot field level missing vals
+                    types = [t for t in prop["type"] if t!="null"]
+                    # NOTE: frictionless doesnt support mixed types
+                    if len(types) > 1:
+                        frictionless_prop["type"] = "any"
+                    else:
+                        frictionless_prop["type"] = prop["type"]
+            else:
+                # NOTE: again frictionless doesn't support mixed (ie oneOf)
+                # enum will be any
+                frictionless_prop["type"] = "any"
 
-        if return_type=='file':\
-            return validated_data.to_csv(self.type+".tsv",sep='\t')
+            # constraints
+            frictionless_constraints = {}
+            ## enum
+            if prop.get("enum"):
+                frictionless_contraints.update({"enum":prop["enum"]})
+            ## required
+            if propname in self.required:
+                frictionless_constraints.update({"required":True})
+            
+            if frictionless_constraints:
+                frictionless_prop.update(frictionless_constraints)
+            
+            frictionless_schema["fields"].append(frictionless_prop)
 
-        if return_type=="records":
-            return validated_data.to_dict(orient="records")
-        elif return_type=='tsv':
-            return validated_data.to_csv(sep="\t")
+    def validate(self,df):
+        ''' 
+        validates against node's frictionless schema. Note,
+        in sheepdog data model, record must be present in parent node.
+        Since this doesn't account for parent node, wont detect.
+        '''
+        cols = [p.get("name") for p in self.frictionless_schema["fields"]]
+        data = df.filter(items=cols) #reorder fields
+        if "submitter_id" in data.columns:
+            data.set_index("submitter_id",inplace=True)
+        report = Resource(data,schema=Schema(self.frictionless_schema)).validate()
+        return report
+    def submit(self,df,program,project,credential_path=None):
+        """ validates (externally of commons)
+        a pandas dataframe of transformed sheepdog records
+        and if valid, will submit to the gen3 graph model 
+        associated with the given credentials, program and project
+        """ 
+        from gen3.auth import Gen3Auth
+        from gen3.submission import Gen3Submission
+
+        if credential_path:
+            self.credential_path = credential_path
+
+        sheepdog = Gen3Submission(Gen3Auth(self.credential_path))
+        report= self.validate(df)
+        if report["valid"]:
+            records = df.reset_index("submitter_id").to_dict(orient="records")
+            return sheepdog.submit_record(program, project, records)
         else:
-            raise Exception("Need to specify one of the return types: file,records,or tsv")
+            print(f"Node `{self.type} not valid. See report below:")
+            raise Exception(report.to_summary())
+    
+    def delete(self,program,project,credentials_path=None):
+        """ 
+        delete all records for a node.
+        This can be useful to ensure that any participants 
+        dropped from dataset are not included in subsequent 
+        dataset. Eg., after further quality control checking
+        or data quarantining.
+        """ 
+        from gen3.auth import Gen3Auth
+        from gen3.submission import Gen3Submission
+
+        if credential_path:
+            self.credential_path = credential_path
+
+        sheepdog = Gen3Submission(Gen3Auth(self.credential_path))
+        sheepdog.delete_node(program, project, self.type)
+        
+
+
+
+
