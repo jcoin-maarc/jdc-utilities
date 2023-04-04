43bccef08207d430e068164ae1534f2175f501fd
mbkranz <kranz-michael@norc.org>
2023-04-04 08:40:04 -0500

    Bug fixes to core measure sheepdog transforms

--- jdc_utils/transforms/sheepdog.py
+++ jdc_utils/transforms/sheepdog.py
@@ -5,18 +5,23 @@ which are common across hubs/datasets
 
 from .curation import collapse_checkall
 import pandas as pd
-   
+
+#NOTE: these are based on the logical representations rather than
+# the physical representations. That is, for example, 
+# while booleanFalse values are physically represented as "Yes", 
+# they are converted to the logical rep in the .to_pandas() method. 
 race_map = {
     "resulting_column_name": "race",
-    "columns_to_labels": {'d3_white': 'White',
-        'd3_black': 'Black or African American',
-        'd3_american_indian': 'American Indian or Alaska Native',
-        'd3_asian': 'Asian',
-        'd3_hawaiian': 'Native Hawaiian or Other Pacific Islander',
-        'd3_other': 'Some other race'},
+    "columns_to_labels": {
+        'race_white': 'White',
+        'race_black': 'Black or African American',
+        'race_AIAN': 'American Indian or Alaska Native',
+        'race_asian': 'Asian',
+        'race_hawaiian_OPI': 'Native Hawaiian or Other Pacific Islander',
+        'race_other': 'Some other race'},
     "multi_checked": 'Some other race',
-    "none_checked": 'Not reported',
-    "checked": 'Yes',
+    "none_checked": "Not reported",
+    "checked": True,
     "fillna": 'Not reported',
     "inplace":False
 }
@@ -25,13 +30,15 @@ recode_map = {
     "visit_type":
         {"Baseline": "Baseline Visit",
         "Follow-up": "Follow-up Visit"},
+    "hispanic_latino":{
+        True:"Yes",
+        False:"No"
+    },
     "gender_id":{
         "Transgender man/trans man/female-to-male (FTM)":"Transgender",
         "Transgender woman/trans woman/male-to-female (MTF)":"Transgender",
         "Genderqueer/gender nonconforming/neither exclusively male nor female":"Gender nonconforming",
-        "Additional gender category (or other)":"Something else",
-        "Missing":"Not reported",
-        "Legitimately skipped":"Not reported"
+        "Additional gender category (or other)":"Something else"
     }
 }
 
@@ -39,11 +46,11 @@ def _choose_gender_field(df):
     # NOTE: all the physical representations of missing values
      # eg., Missing, Legitiamately skipped etc have been converted
      # to NA via the frictionless's Resource method .to_pandas
-    select_gender_col = (
-        df["gender_id"].isna().sum() < df["gender_id_condensed"].isna().sum()
+    select_condensed_col = (
+        df["gender_id"].isna().sum() > df["gender_id_condensed"].isna().sum()
     )
 
-    if select_gender_col:
+    if select_condensed_col:
         return df["gender_id_condensed"]
     else:
         return (
@@ -69,7 +76,7 @@ def to_participant_node(baseline_df):
         "quarter_recruited":baseline_df["quarter_enrolled"],
         "current_client_status":baseline_df["current_study_status"]
     })
-    return node_df
+    return node_df.set_index("submitter_id")
 
 def to_demographic_baseline_node(baseline_df):
     submitter_id = baseline_df.index.get_level_values("jdc_person_id")
@@ -77,10 +84,14 @@ def to_demographic_baseline_node(baseline_df):
         "participants.submitter_id":submitter_id,
         "submitter_id":submitter_id,
         "gender":_choose_gender_field(baseline_df),
-        "hispanic":baseline_df["hispanic_latino"],
+        "hispanic":(
+            baseline_df["hispanic_latino"]
+            .replace(recode_map["hispanic_latino"])
+            .fillna("Not reported")
+        ),
         "race":collapse_checkall(baseline_df,**race_map),
     })
-    return node_df
+    return node_df.set_index("submitter_id")
 
 def to_enrollment_node(baseline_df):
     submitter_id = baseline_df.index.get_level_values("jdc_person_id")
@@ -88,7 +99,7 @@ def to_enrollment_node(baseline_df):
         "participants.submitter_id":submitter_id,
         "submitter_id":submitter_id
     })
-    return node_df
+    return node_df.set_index("submitter_id")
 
 def to_time_point_node(time_point_df):
     get_lvl = time_point_df.index.get_level_values
@@ -103,7 +114,7 @@ def to_time_point_node(time_point_df):
             "submitter_id":submitter_id,
             "visit_type":time_point_df["visit_type"].replace(recode_map["visit_type"])
         })
-    return node_df
+    return node_df.set_index("submitter_id")
 
 def to_baseline_nodes(baseline_df):
     return {
