bf8d6edeea00e32d33341590a9ad50a76ed51b71
mbkranz <kranz-michael@norc.org>
2023-04-07 12:40:54 -0500

    Update version number and added author

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_namespace_packages
 from pathlib import Path
+from version import __version__
 requirements = Path("requirements.txt").open().readlines()
 setup(
     name='jdc-utils',
-    version='0.0.1',
-    author='Phil Schumm',
-    author_email='pschumm@uchicago.edu',
+    version=__version__,
+    author='Michael Kranz,Phil Schumm',
+    author_email='kranz-michael@norc.org,pschumm@uchicago.edu',
     description='Utilities for preparing and submitting data to the JDC',
     url='https://rcg.bsd.uchicago.edu/gitlab/maarc/dasc/jdc-utilities',
     packages=find_namespace_packages(where='.'),
