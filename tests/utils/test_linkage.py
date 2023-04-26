1b6a0df94b8fc6433524b91ea603d6180fbb3ff9
mbkranz <kranz-michael@norc.org>
2023-04-26 08:55:10 -0500

    WIP linkage dev

--- tests/utils/test_linkage.py
+++ tests/utils/test_linkage.py
@@ -1,7 +1,33 @@
 """
-fuzzy matching to support data linkage
+fuzzy matching to support
+(1) variable name linkage/harmonization
+and (2) value linkage from frictionless validation report
 """
-import fuzz
+import json
+from pathlib import Path
+
+from jdc_utils.schema import core_measures
+from thefuzz import process
+
+# read in and flatten core measure schemas
+cdes = list(core_measures.baseline.fields)
+cdes += list(core_measures.timepoints.fields)
+
+# read in source data elements
+sourcepath = (
+    "C:/Users/kranz-michael/projects/jcoin-nyu/"
+    "mappings/flattened_variables_and_values.json"
+)
+source = json.loads(Path(sourcepath).read_text())
+
+
+#
+def read_common_data_elements():
+    pass
+
+
+def read_variable_level_metadata():
+    pass
 
 
 def match_descriptions():
