1aed0219e08ba1fcaf8d8d76ed520f24ac9d70d2
Michael Kranz <mbkranz@gmail.com>
2024-01-22 18:37:27 -0600

    Change branch name for requests

--- jdc_utils/core_measures/encodings.py
+++ jdc_utils/core_measures/encodings.py
@@ -3,7 +3,7 @@
 import requests
 import yaml
 
-branch = "master"
+branch = "main"
 url = f"https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/{branch}/encodings/"
 fields = yaml.safe_load(requests.get(url + "encodings.yaml").content)
 reserve = yaml.safe_load(requests.get(url + "reserve_codes.yaml").content)
--- jdc_utils/core_measures/schemas.py
+++ jdc_utils/core_measures/schemas.py
@@ -1,7 +1,7 @@
 """Easy access to latest versions of JCOIN schema"""
 from frictionless import Schema
 
-branch = "master"
+branch = "main"
 schemadir = f"https://raw.githubusercontent.com/jcoin-maarc/JCOIN-Core-Measures/{branch}/schemas/"
 baseline = Schema(schemadir + "table-schema-baseline.json")
 timepoints = Schema(schemadir + "table-schema-time-points.json")
